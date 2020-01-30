e427bf6cfa84e75b484648f2037ea9b23c397981
Kevem Lennisk Almeida Andrade <keven.almeida.andrade@gmail.com>
2020-01-30 01:40:57 -0300

    Atualiza mensagem inicial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>A Nigéria é o país com maior população negra do mundo.</p>
+        <p>A Nigéria é o país com maior população negra no mundo.</p>
         <p>O secundo maior, é o Brasil.</p>
       </header>
     </div>
