bcb2a7b23fe7e5ea1111551e217b37ffd3354983
Kevem Lennisk Almeida Andrade <keven.almeida.andrade@gmail.com>
2020-01-30 01:36:16 -0300

    Atualiza mensagem inicial e favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="bigtext">Afrikans</p>
+        <p>A Nigéria é o país com maior população negra do mundo.</p>
+        <p>O secundo maior, é o Brasil.</p>
       </header>
     </div>
   );
