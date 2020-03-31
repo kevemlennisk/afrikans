0d5ffaa810586dc5ed93e12deea9503569540cec
Kevem Lennisk Almeida Andrade <keven.almeida.andrade@gmail.com>
2020-03-31 01:20:03 -0300

    Pushing to test Git Actions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>A Nigéria é o país com maior população negra no mundo.</p>
-        <p>O segundo maior, é o Brasil.</p>
+        <p>O segundo maior, é o Brasil. </p>
       </header>
     </div>
   );
